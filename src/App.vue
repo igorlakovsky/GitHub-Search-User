f15f3a8ab1a70b06cc93f53e1d9c5b2d5c8f369f
igorlakovsky <igorlakovsky@yandex.ru>
2023-07-20 04:08:51 +0400

    @vue/cli delete + webpack config

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Button type="primary">Поиск</Button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Button } from 'ant-design-vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Button },
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import 'ant-design-vue/dist/antd.css'
+
+import Antd from 'ant-design-vue'
+import App from './App.vue'
+import Vue from 'vue'
+
+Vue.config.productionTip = false
+
+Vue.use(Antd)
+
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app')
